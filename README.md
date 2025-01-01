fd3fa2ea093d0c3d60c03432040fd4f373d98f5c
vishnupv863 <vishnupv863@gmail.com>
2025-01-01 22:55:47 +0530

    connected to mongodb

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 `npm install`, 
 `npm install express`
+
+vishnupv863
+qSeLaE74buSBaYC6
+mongodb+srv://vishnupv863:<db_password>@cluster0.uknj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import MoviesRoutes from './routes/movies.routes.js'
+import connectDB from './lib/db.js' 
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res) => {
-    res.json({message:'hello developers'})
-});
+// Connect mongodb server
+connectDB();
+
 
 app.listen(PORT, () => {
     console.log(`listening on port at http://localhost:${PORT}`)
